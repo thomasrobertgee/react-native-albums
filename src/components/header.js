42e8edf8b94c20f1f0a6d73d516351c875915952
Thomas Gee <thomasrobertgee@gmail.com>
2017-05-18 19:49:34 +1000

    completed first section of app, got boilerplate code down, next is styling fundamentals

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+// Import libraries to help create a component
+import React from 'react';
+import { Text } from 'react-native';
+
+// Create a component
+const Header = () => {
+  return <Text>Albums</Text>;
+};
+
+// Make the component available to other parts of the app
+export default Header;
